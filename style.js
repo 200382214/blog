5c0a56dcd329ff8feb47d40eb800452ddbef07d6
DhvanitMerchant <31934916+200382214@users.noreply.github.com>
2018-01-04 20:51:48 -0500

    Update style.js

--- style.js
+++ style.js
@@ -1,17 +1,8 @@
-anime.timeline({loop: true})
-.add({
-  targets: '.ml15 .word',
-  scale: [14,1],
-  opacity: [0,1],
-  easing: "easeOutCirc",
-  duration: 800,
-  delay: function(el, i) {
-    return 800 * i;
-  }
-}).add({
-  targets: '.ml15',
-  opacity: 0,
-  duration: 1000,
-  easing: "easeOutExpo",
-  delay: 1000
-});
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
